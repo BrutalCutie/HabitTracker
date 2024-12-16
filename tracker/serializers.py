64594ced01cb2bacd8e0fe1d169144d01e85e7f5
tiken14 <tiken14@live.com>
2024-12-16 12:30:12 +0500

    Настройка urls & views для User и Habit.

---
+++ tracker/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from tracker.models import Habit
+
+
+class HabitSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Habit
+        fields = "__all__"
