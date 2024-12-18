853fa6ef239089d0bdec0b96c432910e533f782d
tiken14 <tiken14@live.com>
2024-12-18 17:54:48 +0500

    Часть тестирования. Результат 90%. Дописать если в дальнейшем будет > 85%

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from tracker.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+    user = None
+
+    def setUp(self):
+        self.user = User.objects.create(
+            telegram_id=1111,
+            password='testuser',
+            username='testuser',
+            email='testuser@testuser.ru'
+        )
+        self.client.force_authenticate(user=self.user)
+
+    def test_create_habit(self):
+        data = {
+            "action": "Выпить воды",
+            "publish_to_all": True,
+            "reward": "Скушать печенюшку",
+            "time": "10-00",
+            "time_to_complete": 120,
+            "place": "Работа"
+
+        }
+
+        response = self.client.post(
+            path='/habits/',
+            data=data
+        )
+        self.assertTrue(
+            Habit.objects.all().exists()
+        )
+        self.assertEqual(
+            response.json(),
+            {'id': 1, 'place': 'Работа', 'time': '10:00:00', 'action': 'Выпить воды', 'is_nice_habit': False,
+             'periodicity': 'every_day', 'reward': 'Скушать печенюшку', 'time_to_complete': '00:02:00',
+             'publish_to_all': True, 'owner': 1, 'chained_habit': None}
+
+        )
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+
+    def test_create_user(self):
+        self.user = User.objects.create(
+            telegram_id=1111,
+            password='testuser',
+            username='testuser',
+            email='testuser@testuser.ru'
+        )
+
+    def test_create_habit(self):
+        data = {
+            "telegram_id": 1111,
+            "password": 'testuser',
+            "username": 'testuser',
+            "email": 'testuser@testuser.ru'
+        }
+
+        response = self.client.post(
+            path='/users/',
+            data=data
+        )
+        self.assertTrue(
+            User.objects.all().exists()
+        )
+        # Делаем заглушки, потому, что пароль хэшируется, а дата меняется
+        mock_password = response.json()['password']
+        mock_date_joined = response.json()['date_joined']
+
+        self.assertEqual(
+            response.json(),
+            {'id': 1, 'password': mock_password,
+             'last_login': None, 'is_superuser': False, 'first_name': '', 'last_name': '', 'is_staff': False,
+             'is_active': False, 'date_joined': mock_date_joined, 'username': 'testuser',
+             'email': 'testuser@testuser.ru', 'telegram_id': 1111, 'groups': [], 'user_permissions': []}
+
+        )
